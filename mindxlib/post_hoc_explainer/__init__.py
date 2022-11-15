6cee92360f264300b66d165436986759f989902f
jingbang.yjb <jingbang.yjb@alibaba-inc.com>
2022-11-15 18:57:23 +0800

    Add shap permutation API.
    Signed-off-by: jingbang.yjb <jingbang.yjb@alibaba-inc.com>

--- mindxlib/post_hoc_explainer/__init__.py
+++ mindxlib/post_hoc_explainer/__init__.py
@@ -1,2 +1,2 @@
-from ._shap import KernelExplainer, GradientExplainer, DeepExplainer, TreeExplainer
+from ._shap import KernelExplainer, GradientExplainer, DeepExplainer, TreeExplainer, PermutationExplainer
 from ._lime import LimeImageExplainer, LimeTabularExplainer, LimeTextExplainer
--- mindxlib/post_hoc_explainer/_shap.py
+++ mindxlib/post_hoc_explainer/_shap.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
 from abc import ABC
 
+import numpy as np
+
 import shap
 from mindxlib.base import PostHocWhiteBoxBase, PostHocBlackBoxBase
 
@@ -148,3 +150,73 @@ class LinearExplainer(PostHocWhiteBoxBase):
         Explain one or more input instances.
         """
         return (self.explainer.shap_values(*argv, **kwargs))
+
+
+class PermutationExplainer():
+    """
+    This method approximates the Shaply values by iterating through permutations of the inputs.
+    """
+
+    def __init__(self, model, n_permutations: int=1024, *argv, **kwargs):
+        """
+        Initialize shap permutationexplainer object.
+        """
+        # super(PermutationExplainer, self).__init__(*argv, **kwargs)
+
+        # self.explainer = shap.LinearExplainer(*argv, **kwargs)
+        self.model = model
+        self.n_permutations = n_permutations
+
+
+    def set_parameters(self, *argv, **kwargs):
+        """
+        Optionally, set parameters for the explainer.
+        """
+        pass
+
+    def explain_instance(self, Xt, Xb, groups: np.ndarray, seed: int=(1<<31)-1, *argv, **kwargs):
+        """
+        Explain one or more input instances.
+
+        Args:
+            Xt: (M, D) array, targets to explain.
+            Xb: (M, D) array, baselines.
+            groups: (N, ) array of indices, group of feature to change together.
+
+        Returns:
+            contribs: (M, D) array.
+        """
+        self.rng = np.random.default_rng(seed)
+        groups = groups + 1
+        M, D = Xt.shape
+        # assert D <= 20
+        N = len(groups)
+        P = self.n_permutations
+
+        perms = np.stack([self.rng.permutation(N) for _ in range(P // 2)], axis=0)
+        perms = np.concatenate((perms, perms[:, ::-1]), axis=0)
+
+        index_perms = groups[perms]
+        tril = np.tril(np.ones((N + 1, N), dtype=int), k=-1)
+        partial = index_perms[:,np.newaxis,:]* tril # (P, D+1, D)
+        
+        S = P * (N + 1)
+        partial = np.apply_along_axis(np.concatenate, 1, partial.reshape(S, N))
+        masks = np.zeros((partial.shape[0],partial.shape[1]+1), dtype=float)
+        np.put_along_axis(masks, partial, 1, axis=-1)
+        masks = masks[:, np.newaxis, 1:]
+
+        mixed = Xt[np.newaxis, ...] * masks + Xb[np.newaxis, ...] * (1 - masks)
+        preds = self.model.predict(mixed.reshape(S * M, D)).reshape(P, N+1, M)
+
+        gains = preds[:, 1:, :] - preds[:, :-1, :]  # (P, N, M)
+        contribs = np.zeros_like(gains)
+        np.put_along_axis(contribs, perms[..., np.newaxis], gains, axis=-2)
+        contribs = np.mean(contribs, axis=0)
+        return np.transpose(contribs)
+
+    # def explain_instance(self, *argv, **kwargs):
+    #     """
+    #     Explain one or more input instances.
+    #     """
+    #     return (self.explainer.shap_values(*argv, **kwargs))
