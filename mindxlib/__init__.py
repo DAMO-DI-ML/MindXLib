c1de981bd6bb1e67764e590a426188a289fd18d2
Xinyue <guxinyue.gxy@alibaba-inc.com>
2025-04-07 17:55:00 +0800

    lazy imports

--- mindxlib/__init__.py
+++ mindxlib/__init__.py
@@ -1,12 +1,31 @@
 __version__ = "0.1.0"
 
-from mindxlib.explainers.rules.rulelist.rulelist_SSRL import SSRL
-from mindxlib.explainers.rules.ruleset import Diver
-from mindxlib.explainers.rules.ruleset import DrillUp
-from mindxlib.explainers.rules.ruleset import RuleSetImb
-from mindxlib.explainers.rules.ruleset import RuleSet
-from mindxlib.explainers.timeseries import FDTempExplainer
-from mindxlib.explainers.interactive_gam.gam import GAM
-from mindxlib.explainers.common_library_attribution.shap import ShapExplainer
+def __getattr__(name):
+    """Lazy import mechanism"""
+    if name == 'SSRL':
+        from mindxlib.explainers.rules.rulelist.rulelist_SSRL import SSRL
+        return SSRL
+    elif name == 'GAM':
+        from mindxlib.explainers.interactive_gam.gam import GAM
+        return GAM
+    elif name == 'ShapExplainer':
+        from mindxlib.explainers.common_library_attribution.shap import ShapExplainer
+        return ShapExplainer
+    elif name == 'Diver':
+        from mindxlib.explainers.rules.ruleset import Diver
+        return Diver
+    elif name == 'DrillUp':
+        from mindxlib.explainers.rules.ruleset import DrillUp
+        return DrillUp
+    elif name == 'RuleSetImb':
+        from mindxlib.explainers.rules.ruleset import RuleSetImb
+        return RuleSetImb
+    elif name == 'RuleSet':
+        from mindxlib.explainers.rules.ruleset import RuleSet
+        return RuleSet
+    elif name == 'FDTempExplainer':
+        from mindxlib.explainers.timeseries import FDTempExplainer
+        return FDTempExplainer
+    raise AttributeError(f"module '{__name__}' has no attribute '{name}'")
 
-__all__ = ['SSRL','FDTempExplainer','Diver','DrillUp','RuleSetImb','RuleSet','GAM', 'ShapExplainer']
+__all__ = ['SSRL', 'FDTempExplainer', 'Diver', 'DrillUp', 'RuleSetImb', 'RuleSet', 'GAM', 'ShapExplainer']
